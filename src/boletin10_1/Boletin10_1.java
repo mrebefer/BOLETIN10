c9007e34f65841f476fc2c55e82bbb66b39d2d6a
mrebelladofernandez <mrebelladofernandez@nautilus18.danielcastelao.org>
2018-11-21 12:23:53 +0100

    first commit

---
+++ src/boletin10_1/Boletin10_1.java
@@ -0,0 +1,24 @@
+/*
+ * Codifica un programa que permita o seguinte xogo para adiviñar un número.
+ * O primeiro xogador teclea (utilizando a clase JoptionPane),un número a adiviñar 
+ * (entre 1 e 50) e o número máximo de intentos que ten o segundo xogador para dar 
+ * con el.O programa irá sacando por pantaia mensaxes que orienten o segundo xogador
+ * sobre se o número tecleado e maior ou menor que o número a adiviñar.
+ * Fai o programa repetitivo.
+ */
+package boletin10_1;
+
+/**
+ *
+ * @author mrebelladofernandez
+ */
+public class Boletin10_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
